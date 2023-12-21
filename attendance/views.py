a375db2014c89ca17360cde91be343cfccfc36e0
HaruNakano <haru0117.nkn@gmail.com>
2023-12-22 01:50:42 +0900

    get total working hours

--- attendance/views.py
+++ attendance/views.py
@@ -2,7 +2,7 @@ from django.http.response import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from .models import Attendances
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 class HomeView(LoginRequiredMixin, TemplateView):
     #表示するテンプレート
@@ -78,7 +78,21 @@ class AttendanceRecords(LoginRequiredMixin, TemplateView):
             attendance_time__year = search_year,
             attendance_time__month = search_month
         ).order_by('attendance_time')
- 
+        
+        total_work_hours = timedelta()
+        
+        for attendance in month_attendances:
+            attendance_time = attendance.attendance_time
+            leave_time = attendance.leave_time
+            
+            if leave_time and attendance_time:
+                work_duration = leave_time - attendance_time
+                total_work_hours += work_duration
+                
+        total_hours, remainder = divmod(total_work_hours.seconds, 3600)
+        total_minutes, _ = divmod(remainder, 60)
+        total_work_hours_str = f"{total_hours:02}:{total_minutes:02}"
+        
         # context用のデータに整形
         attendances_context = []
         for attendance in month_attendances:
@@ -96,6 +110,8 @@ class AttendanceRecords(LoginRequiredMixin, TemplateView):
             }
             attendances_context.append(day_attendance)
  
-        context = {'attendances': attendances_context}
+        context = {'attendances': attendances_context,
+                   'total_work_time': total_work_hours_str
+                }
         # Templateにcontextを含めてレスポンスを返す
         return self.render_to_response(context)
